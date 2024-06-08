dac280eea3c803407ded6bdce31c1bbf7c10d748
Hamdani <hamdani@synergizetech.dev>
2024-06-08 20:25:14 +0800

    feat: Add next layer of Authorization check

--- src/routes/admin/index.ts
+++ src/routes/admin/index.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express'
 
 import { DashboardController, LoginController } from '@/controller/admin'
-import { auth, validate } from '@/middlewares'
+import { validate } from '@/middlewares'
 import { authSchema } from '@/schemas/admin'
 
 const router = Router()
 
 router.post('/login', [validate(authSchema)], LoginController)
-router.get('/dashboard', [auth], DashboardController)
+router.get('/dashboard', DashboardController)
 
 export default router
